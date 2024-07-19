f6066ec65951452a76381b63bf51d0ec606e1f20
Shreya S Poojary <96349610+Shreyapoojary14s@users.noreply.github.com>
2024-07-19 12:40:16 +0530

    Compiled

---
+++ ASCIIValue.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ASCIIValue {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a character : ");
+        char a=scanner.next().charAt(0);
+        ascii(a); //call function
+        scanner.close();
+    }
+    public static void ascii(char a){
+        int asciiValue = (int) a; //casting char into int
+        System.out.println("ASCII value of '"+a+"' is :"+asciiValue);
+    }
+}
