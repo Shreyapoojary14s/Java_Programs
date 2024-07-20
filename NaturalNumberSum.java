e317e9e9f98df1f9a88003811f0fa62700d70a91
Shreya S Poojary <96349610+Shreyapoojary14s@users.noreply.github.com>
2024-07-20 16:52:16 +0530

    Basic

--- NaturalNumberSum.java
+++ NaturalNumberSum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NaturalNumberSum {
+public class NaturalNumberSum{
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter the number of natural numbers :");
